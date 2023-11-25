6d667cb7489bfe9cba80a7ba2db7add64ef93ab4
unknown <rafaellemosmr@gmail.com>
2023-11-25 15:32:34 -0300

    first commit

---
+++ src/main/java/com/fatec/ComandaGO/ComandaGoApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.ComandaGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandaGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/ComandaGO/ComandaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.ComandaGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
